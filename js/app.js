edea590df815099018b20dc7c7070413c132206d
Angel Ivanov Mac <angel.iwanov@gmail.com>
2022-05-12 15:20:00 +0300

    BuildNavigationMenu

--- js/app.js
+++ js/app.js
@@ -22,15 +22,28 @@
  * Define Global Variables
  * 
 */
+const fragment = document.createDocumentFragment();
 
-
+const sections = document.querySelectorAll('section');
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
+function createNavItemHTML(id, name){
+    const NavItemHTML = `<a class="navbar__link" data-id="${id}">${name}</a>`;
+    return NavItemHTML;
+};
 
-
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
 
 /**
  * End Helper Functions
@@ -39,7 +52,17 @@
 */
 
 // build the nav
-
+function buildNavigation(){
+    for (let i=0; i < sections.length; i++){
+        const menuItem = document.createElement('li');
+        const sectionName = sections[i].getAttribute('data-nav')
+        const sectionId = sections[i].getAttribute('id')
+        menuItem.innerHTML = createNavItemHTML(sectionId, sectionName)
+        fragment.appendChild(menuItem);
+    }
+    const navBarList = document.getElementById('navbar__list')
+    navBarList.appendChild(fragment);
+}
 
 // Add class 'active' to section when near top of viewport
 
