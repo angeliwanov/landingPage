2c98bf97305c07791df0408e88d9d3071f6d7812
Angel Ivanov Mac <angel.iwanov@gmail.com>
2022-05-19 16:01:36 +0300

    Added event listeners

--- README.md
+++ README.md
@@ -10,4 +10,53 @@ The starter project has some HTML and CSS styling to display a static version of
 
 To get started, open `js/app.js` and start building out the app's functionality
 
-For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+The following functionalities have been implementer:
+
+1. Start by linking your app.js. where should this file go based on your present knowledge? We’ll test some other locations later.
+- <script src="app.js"> added at the end of body, after DOM is loaded.
+
+2. Build out your HTML and at least 4 content sections. 
+- added 4th and 5th sections in index.html
+
+2.1 Take a quick look at all the HTML elements in index.html. Note the values for their id, class, and data attributes. To manipulate the DOM, you'll be using many of the tools and methods you've learned on these elements (and on those that you will create).
+-data-* attribute
+
+2.2 Which data structure can you use to store these sections? This data structure can represent all sections for your page, so it might be a good idea to save it to a variable.
+- createDocumentFragment() 
+
+2.3 How you would iterate (i.e., loop) over this data structure?
+- querySelectorAll(selectors)
+
+2.4 Think about how you can create, say, an unordered list (i.e., bulleted list) in HTML from this structure, and where you be placing that list.
+- function createNavItemHTML(id, name)
+
+2.5 Think about how you’ll test whether a section is in the viewport.
+- getBoundingClientRect();
+
+3. Build the navigation menu
+- loop over the sections and for each section:
+    - create li element
+    - get id and data-nav
+    - use helper function createNavItemHTML() to create link 
+    - append to fragment
+- select navBarList
+- append fragement to navBarList
+
+
+4. Add functionality to distinguish the section in view.
+- loop over all sections
+- check if section is inViewport
+- if true - add classList "active"
+- if no - remove classList "active"
+
+5. Add the functionality to scroll to sections
+- if the event Nodename is 'a' (link)
+- then get the section id
+- get get section
+- scroll to the section
+
+6. Add events
+- scroll to section on click using scrollSection()
+- set section as active using sectionInViewport()
+
+7. Check for responsiveness using Google Developer Tools
--- js/app.js
+++ js/app.js
@@ -30,18 +30,18 @@ const sections = document.querySelectorAll('section');
  * Start Helper Functions
  * 
 */
-function createNavItemHTML(id, name){
-    const NavItemHTML = `<a class="navbar__link" data-id="${id}">${name}</a>`;
-    return NavItemHTML;
-};
+function createNavItemHTML (id, name) {
+    const navItemHTML =`<a class="menu__link" data-id="${id}">${name}</a>`;
+    return navItemHTML;
+}
 
-function isInViewport(element) {
+function isInViewport (element) {
     const rect = element.getBoundingClientRect();
     return (
         rect.top >= 0 &&
         rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        rect.bottom <= (window.innerHeight || window.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || window.documentElement.clientWidth)
     );
 }
 
@@ -52,23 +52,37 @@ function isInViewport(element) {
 */
 
 // build the nav
-function buildNavigation(){
-    for (let i=0; i < sections.length; i++){
-        const menuItem = document.createElement('li');
-        const sectionName = sections[i].getAttribute('data-nav')
-        const sectionId = sections[i].getAttribute('id')
-        menuItem.innerHTML = createNavItemHTML(sectionId, sectionName)
-        fragment.appendChild(menuItem);
+function buildNavMenu () {
+    for (let i=0; i<sections.length; ++i) {
+        const navMenuItem = document.createElement('li');
+        const sectionName = sections[i].getAttribute('data-nav');
+        const sectionId = sections[i].getAttribute('id');
+        navMenuItem.innerHTML = createNavItemHTML(sectionId, sectionName);
+        fragment.appendChild(navMenuItem);
     }
-    const navBarList = document.getElementById('navbar__list')
+    const navBarList = document.getElementById('navbar__list');
     navBarList.appendChild(fragment);
 }
 
 // Add class 'active' to section when near top of viewport
-
+function sectionInView() {
+    for (let i=0; i<sections.length; ++i) {
+        if (isInViewport(sections[i])) {
+            sections[i].classList.add('your-active-class');
+        } else {
+            sections[i].classList.remove('your-active-class');
+        }
+    }
+}
 
 // Scroll to anchor ID using scrollTO event
-
+function scrollSection (event) {
+    if (event.target.nodeName == 'A') {
+        const sectionId = event.target.getAttribute('data-id');
+        const section = document.getElementById(sectionId);
+        section.scrollIntoView({behavior: "smooth"});
+    }
+}
 
 /**
  * End Main Functions
@@ -76,10 +90,16 @@ function buildNavigation(){
  * 
 */
 
-// Build menu 
+// Build menu
+buildNavMenu();
 
 // Scroll to section on link click
-
+const navBarMenu = document.getElementById('navbar__list');
+navBarMenu.addEventListener('click', function(event) {
+    scrollSection(event);
+});
 // Set sections as active
-
+document.addEventListener('scroll', function() {
+    sectionInView();
+});
 
